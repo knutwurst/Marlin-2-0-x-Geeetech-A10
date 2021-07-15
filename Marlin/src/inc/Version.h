9ae3518a7bd2049d35e9e647fd1e262301c85296
Knutwurst <36196269+knutwurst@users.noreply.github.com>
2021-07-15 10:21:03 +0200

    Initial commit for GEETECH A10 Printer with 3D Touch

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2021-06-27"
+  #define STRING_DISTRIBUTION_DATE "2021-07-15"
 #endif
 
 /**
@@ -73,7 +73,7 @@
  * Define a generic printer name to be output to the LCD after booting Marlin.
  */
 #ifndef MACHINE_NAME
-  #define MACHINE_NAME "3D Printer"
+  #define MACHINE_NAME "GEEETECH A10 Pro"
 #endif
 
 /**
@@ -82,7 +82,7 @@
  * providing the source code to your customers.)
  */
 #ifndef SOURCE_CODE_URL
-  #define SOURCE_CODE_URL "github.com/MarlinFirmware/Marlin"
+  #define SOURCE_CODE_URL "github.com/knutwurst"
 #endif
 
 /**
@@ -97,7 +97,7 @@
    * documentation about a specific Marlin release. Displayed in the Info Menu.
    */
 #ifndef WEBSITE_URL
-  #define WEBSITE_URL "marlinfw.org"
+  #define WEBSITE_URL "github.com/knutwurst"
 #endif
 
 /**
